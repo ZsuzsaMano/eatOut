2940ecd15acb3cc18b43e674ed46295db3cf2970
Mano <manoaway@gmail.com>
2019-11-26 08:47:53 +0100

    Updates

---
+++ precache-manifest.d429f2841a76a65a4ed02101f3c27281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa59f653903a665b90600a23c6a575d1",
+    "url": "/eatOut/index.html"
+  },
+  {
+    "revision": "149e102ec73aa5576d04",
+    "url": "/eatOut/static/css/main.2ba589f2.chunk.css"
+  },
+  {
+    "revision": "c454001183ebd91012b7",
+    "url": "/eatOut/static/js/2.efcae61a.chunk.js"
+  },
+  {
+    "revision": "149e102ec73aa5576d04",
+    "url": "/eatOut/static/js/main.c4f7b533.chunk.js"
+  },
+  {
+    "revision": "3b4bd4e988de55f71da7",
+    "url": "/eatOut/static/js/runtime~main.4049bf71.js"
+  }
+]);
